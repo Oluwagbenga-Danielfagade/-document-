9a85e906f8bc7aee48a4d7bae08bbb42d880cd72
Oluwagbenga-Danielfagade <168472297+Oluwagbenga-Danielfagade@users.noreply.github.com>
2024-04-29 12:22:55 -0700

    Delete Ecommerce cars site template directory

--- Ecommerce cars site template/Ecommerce Homepage.js
+++
@@ -1,6 +0,0 @@
-function Signin(){
-    window.open("Ecommerce Signin.html");
-}
-function Aboutus(){
-    window.open("Ecommerce About us.html");
-}
--- Ecommerce cars site template/Ecommerce.js
+++
@@ -1,3 +0,0 @@
-function window(){
-    window.open("contactus");
-}
